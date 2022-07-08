1c84b6141acdd201488e93513ebdc5370106ffb1
Faran <farantgh@gmail.com>
2022-07-08 10:52:50 +0430

    fix small bugs

--- backup.py
+++ backup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.9
+#!/usr/bin/python3.10
 
 import datetime
 import os
@@ -6,52 +6,56 @@ import json
 import argparse
 
 parser = argparse.ArgumentParser(prog="python backup.py", )
-parser.add_argument("-s", "--settings", help="file path that contain settings. in json format.", nargs='?', default="settings.json")
+parser.add_argument(
+    "-s", "--settings",
+    help="file path that contain settings. in json format.",
+    nargs='?', default="settings.json")
 
 
 class CustomExceptions(Exception):
-	pass
+    pass
 
 
 def load_settings(settings_file):
-	settings = ""
-	with open(settings_file, "r") as file:
-		settings = file.read()
-	try:
-		data = json.loads(settings)
-		if data["main_path"][-1] != "/":
-			data["main_path"] = data["main_path"] + "/"
-		data_list = [data["app_name"], data["main_path"], data["tables"]]
-	except KeyError as error:
-		raise CustomExceptions("ERROR: {0} is not a valid setting key.".format(error.message))
-	return data_list
+    settings = ""
+    with open(settings_file, "r", encoding="utf-8") as file:
+        settings = file.read()
+    try:
+        data = json.loads(settings)
+        if data["main_path"][-1] != "/":
+            data["main_path"] = data["main_path"] + "/"
+        data_list = [data["app_name"], data["main_path"], data["tables"]]
+    except KeyError as error:
+        raise CustomExceptions(f"ERROR: {error.message} is not a valid setting key.") from error
+    return data_list
 
 
 def backup(app_name, main_path, tables):
-	os.chdir(main_path)
-	now = datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S")
-	commands = {}
-	os.mkdir(now)
-	os.chdir(now)
-	print("\u001b[34m" + "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+" + "\033[0m")
+    os.chdir(main_path)
+    now = datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S")
+    commands = {}
+    os.mkdir(now)
+    os.chdir(now)
+    print("\u001b[34m" + "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+" + "\033[0m")
 
-	for table in tables:
-		commands[table] = "heroku pg:psql -c \"\\copy (select * from {0}) to \'./b-{0}.csv\' with csv\" -a {1}".format(table, app_name)
-	commands["django-dumpdata"] = "heroku run python manage.py dumpdata -a {} | json_pp > b-dumpdata-python.json".format(app_name)
+    for table in tables:
+        commands[table] = f"heroku pg:psql -c \"\\copy (select * from {table}) to \'./b-{table}.csv\' with csv\" -a {app_name}"
+    commands["django-dumpdata"] = f"heroku run python manage.py dumpdata -a {app_name} > b-dumpdata-python.json"
 
-	for command in commands:
-		print("\033[92m" + f"\n+++---------- '{command}' ----------+++\n" + "\033[0m")
-		os.system(commands[command])
+    for name, command in commands.items():
+        print("\033[92m" + f"\n+++---------- '{name}' ----------+++\n" + "\033[0m")
+        os.system(command)
 
-		print("\u001b[34m" + "in: "+ "\033[0m" + "{0}{1}/b-{2}.csv".format(main_path, now, command))
-	print("\nDone.\n\u001b[34m" + "\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+" + "\033[0m\n")
+        print("\u001b[34m" + "in: "+ "\033[0m" + f"{main_path}{now}/b-{name}.csv")
+    print("\nDone.\n\u001b[34m" +
+    "\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+" + "\033[0m\n")
 
 
 def main():
-	args = parser.parse_args()
-	data = load_settings(settings_file=args.settings)
-	backup(data[0], data[1], data[2])
+    args = parser.parse_args()
+    data = load_settings(settings_file=args.settings)
+    backup(data[0], data[1], data[2])
 
 
 if __name__ == '__main__':
-	main()
+    main()
